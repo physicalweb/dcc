c619edf8d4142704b78c86448126c7474ee14c2b
Arnon Zangvil <arnonz@physicalweb.com>
2025-12-16 22:17:20 +0200

    Working skeleton version, public version

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,3 +1,4 @@
+// Forcing removal of local AIDL config to resolve duplicate class error.
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -48,7 +49,4 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
